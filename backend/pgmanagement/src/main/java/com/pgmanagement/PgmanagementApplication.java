2bf9985e7bf225ed3ac0849440c50697ef33914b
narasimha469 <m.nnaidu756@gmail.com>
2026-02-20 20:30:29 +0530

    Added Spring Boot backend project

---
+++ backend/pgmanagement/src/main/java/com/pgmanagement/PgmanagementApplication.java
@@ -0,0 +1,15 @@
+package com.pgmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgmanagementApplication.class, args);
+		
+		System.out.println("MY APPLICATION STARTED......");
+	}
+
+}
